c6a62e4a9b8c378bee6b2168e3ebcde91c07be2f
jahnavireddy1709 <moramreddyjahnavi@gmail.com>
2024-07-28 15:00:54 +0530

    added attendance and classes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Attendance from "./Attendance";
+import Classes from "./Classes";
 
 
 function App() {
   return (
     <>
-    <h1>Kodnest App</h1>
+    <h1>Kodnest App 
+    <Classes/>
+    <Attendance/>
+    </h1>
+    
+
     </>
         
   );
